72214378530154055619dc41fd64af60dbcb137e
hjee02018 <78251977+hjee02018@users.noreply.github.com>
2022-03-29 18:31:04 +0900

    modify app.js

--- App.js
+++ App.js
@@ -4,12 +4,20 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 import HomeScreen  from './Components/HomeScreen'
-
+import Login from './Components.Login'
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
+   <NavigationContainer>
+   <Stack.Navigator initialRouteName="Start">
+        <Stack.Screen
+          name="Start"
+          component={Start}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen name="Login" component={Login} options={{title: '로그인',headerBackTitle: '',headerShadowVisible: false}}/>
     <View style={styles.container}>
       <HomeScreen></HomeScreen>
     </View>
