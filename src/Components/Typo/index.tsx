1880f56b405caf85e20d3b5cc7fe1458dc59d77a
ysg81 <qpple6@naver.com>
2022-04-03 01:01:43 +0900

    add : color & font 적용

---
+++ src/Components/Typo/index.tsx
@@ -0,0 +1,20 @@
+import {FunctionComponent} from 'react';
+import {StyleSheet, Text, TextProps} from 'react-native';
+import {typos, TypoType} from '../../Components/Typo/type';
+import {colors} from 'src/Components/_styles/colors'
+
+interface TypoProps extends TextProps{
+    type:TypoType
+}
+
+const Typo: FunctionComponent<TypoProps> = ({type, style, ...rest}) => {
+    return <Text style={[type ? typos[type] : undefined, styles.text, style]} {...rest}/>
+}
+
+const styles = StyleSheet.create({
+    text: {
+        color: colors.BLACK
+    }
+})
+
+export default Typo
