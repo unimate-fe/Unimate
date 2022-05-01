b1d60605b29725e831beaa21f95fab221c69706e
hjee02018 <hjee02018@naver.com>
2022-05-01 16:15:44 +0900

    Add : mbti/interest screen

--- src/screens/RegisterMbtiScreen/index.tsx
+++ src/screens/RegisterMbtiScreen/index.tsx
@@ -1,5 +1,5 @@
 import React, {useCallback,useState, FunctionComponent} from 'react';
-import {View, Text,StyleSheet} from 'react-native';
+import {View, Text,StyleSheet,Pressable} from 'react-native';
 
 // add
 import CheckBox from '@react-native-community/checkbox';
@@ -7,6 +7,8 @@ import useScreenNavigation from '@hooks/useScreenNavigation';
 import DismissKeyboardView from '@components/DismissKeyboardView'
 import TextInput from '@components/Input';
 import Button from '@components/Button';
+import {strings} from '@screens/RegisterMbtiScreen/string';
+import SafeContainer from '@components/SafeContainer';
 
 interface Props {}
 
@@ -24,110 +26,100 @@ const RegisterMbtiScreen: FunctionComponent<Props> =
     const [selectJ, setSelectJ] = useState('');
     const [selectP, setSelectP] = useState('');
   
-      const toInterest = useCallback(() => {
-        navigation.navigate('RegisterInterest');
-      }, [navigation]);
+      // const toInterest = useCallback(() => {
+      //   navigation.navigate('RegisterInterest');
+      // }, [navigation]);
 
     return (
-      <DismissKeyboardView style={{backgroundColor: 'white'}}>
-        <View style={{paddingLeft:30}}>
+      <SafeContainer>
+        <View style={style.base}>
           <Text style={style.pageTitle1}>MBTI를 선택해주세요.</Text>
-        </View>
-        <View style={{paddingLeft:30}}>
           <Text style={style.pageTitle2}>나와 잘 맞는 친구를 찾기 위한 정보예요.</Text>
           <Text style={style.pageTitle2}>정확하게 입력해 주세요!</Text>
-      </View>
-      {/* E ------------ */}
-      <View style={style.inputTopWrapper}>
-        <TextInput
-            style={style.textInput}
-            placeholder="E"
-            value='E'
-            placeholderTextColor="#666"
-            editable={false}
+          
+          
+          <Button
+          type={'Solid-Long'}
+          label={strings.NEXT}
+          onPress={()=>navigation.navigate('RegisterInterest')}
           />
-        <CheckBox
-              // value={selectE}
-              disabled={false}
-              style={style.checkBox}
-              onCheckColor={'#FFB6C7'}
-              onTintColor={'#FFB6C7'}
-            />
-      </View>
-      <View style={style.inputContainer}>
-      <Button type={'Solid-Long'} label={"다음"} onPress={() => navigation.navigate('RegisterInterest')} />
-      </View>
-      </DismissKeyboardView>
+          <Pressable style={style.PassButton} onPress={()=>navigation.navigate('RegisterInterest')}>
+            <Text style={style.PassButtonText}>잘 모르겠어요 Skip </Text>
+          </Pressable>
+        </View>
+      </SafeContainer>
     );
   };
 
   
-const style = StyleSheet.create({
-  pageTitle1: {
-    fontWeight: 'bold',
-    fontSize: 24,
-    color: '#212529',
-    padding: 5,
-  },
-  pageTitle2: {
-    fontSize: 14,
-    color: '#828C94',
-    padding: 5,
-  },
-  textInput: {
-    width: 45,
-    height: 45,
-    padding: 17
-  },
-  checkBox: {
-  },
-  inputTopWrapper: {
-    padding: 7,
-    paddingTop: 48,
-    alignItems: 'center',
-    flexDirection: 'row',
-    justifyContent: 'center',
-  },
-  inputWrapper: {
-    padding: 7,
-    paddingTop: 20,
-    alignItems: 'center',
-    flexDirection: 'row',
-    justifyContent: 'center',
-  },
-  buttonZone: {
-    padding: 37,
-    alignItems: 'center',
-  },
-  inputContainer: {
-    marginTop:20,
-    marginLeft:37,
-    marginRight:37,
-    marginBottom: 20,
-  },
-  NextButton: {
-    width: 315,
-    height: 56,
-    borderRadius: 12,
-    backgroundColor: '#9678FE',
-    padding: 20,
-    alignItems: 'center',
-  },
-  NextButtonText: {
-    fontWeight: 'bold',
-    fontSize: 15,
-    color: '#fff',
-  },
-  PassButton: {
-    width: 315,
-    height: 56,
-    padding: 10,
-    alignItems: 'center',
-  },
-  PassButtonText: {
-    fontSize: 13,
-    color: '#9678FE',
-  },
-});
+  const style = StyleSheet.create({
+    pageTitle1: {
+      fontWeight: 'bold',
+      fontSize: 24,
+      color: '#212529',
+      padding: 5,
+    },
+    base: {
+      paddingTop: 28,
+      paddingHorizontal: 30,
+    },
+    pageTitle2: {
+      fontSize: 14,
+      color: '#828C94',
+      padding: 5,
+    },
+    textInput: {
+      width: 45,
+      height: 45,
+      padding: 17
+    },
+    checkBox: {
+    },
+    inputTopWrapper: {
+      padding: 7,
+      paddingTop: 48,
+      alignItems: 'center',
+      flexDirection: 'row',
+      justifyContent: 'center',
+    },
+    inputWrapper: {
+      padding: 7,
+      paddingTop: 20,
+      alignItems: 'center',
+      flexDirection: 'row',
+      justifyContent: 'center',
+    },
+    buttonZone: {
+      padding: 37,
+      alignItems: 'center',
+    },
+    NextButton: {
+      width: 315,
+      height: 56,
+      borderRadius: 12,
+      backgroundColor: '#9678FE',
+      padding: 20,
+      alignItems: 'center',
+    },
+    checkbox: {
+      transform: [{scaleX: 0.8}, {scaleY: 0.8}],
+    },
+    NextButtonText: {
+      fontWeight: 'bold',
+      fontSize: 15,
+      color: '#fff',
+    },
+    PassButton: {
+      width: 315,
+      height: 56,
+      padding: 10,
+      alignItems: 'center',
+    },
+    PassButtonText: {
+      fontSize: 13,
+      color: '#9678FE',
+    },
+  });
+  
 
 export default RegisterMbtiScreen;
