a4dfe4a9ccdda0caad5086b65569b22b77bd8d69
hjee02018 <hjyang@vision-in.co.kr>
2022-06-13 11:06:02 +0900

    add : 방만들기 api

---
+++ src/screens/RoomSelectGender/index.tsx
@@ -0,0 +1,115 @@
+import React, {FunctionComponent,useState} from 'react';
+import {View, Text, StyleSheet, Pressable} from 'react-native';
+import {colors} from '@components/Styles/colors';
+import SafeContainer from '@components/SafeContainer';
+import Button from '@components/Button';
+import useScreenNavigation from '@hooks/useScreenNavigation';
+
+interface Props {}
+
+const RoomSelectGender: FunctionComponent<Props> = function RoomSelectGender() {
+    const navigation = useScreenNavigation();
+    const [Gender, setGender] = useState(false);
+    const [sameGender, setsameGeder] = useState(false); 
+    
+    const onPress = () => 
+    {
+      if (Gender || sameGender) 
+      {
+        navigation.navigate('RoomSelectGrade');
+      }
+    };
+  return (
+    <SafeContainer style={[{backgroundColor: colors.WHITE}]}>
+      <View style={styles.textContainer}>
+        <Text style={styles.title}>메이트들의 성별을 선택해 주세요.</Text>
+      </View>
+
+{/* progress bar */}
+
+<View style={styles.buttonContainer}>
+        <Pressable
+          style={[
+            styles.NextButton,
+            Gender
+              ? {backgroundColor: colors.PRIMARY.ULTRA_LIGHT}
+              : {backgroundColor: colors.LIGHT_GREY1},
+          ]}
+          onPress={() => {setGender(!Gender); onPress();}}>
+          <Text  style={[
+            styles.NextButtonText,
+            Gender
+              ? {color: colors.PRIMARY.DARK}
+              : {color: colors.GREY2},
+          ]}>상관 없음</Text>
+        </Pressable>
+      </View>
+
+      <View style={styles.buttonContainer}>
+        <Pressable
+          style={[
+            styles.NextButton,
+            sameGender
+              ? {backgroundColor: colors.PRIMARY.ULTRA_LIGHT}
+              : {backgroundColor: colors.LIGHT_GREY1},
+          ]}
+          onPress={() =>{ setsameGeder(!sameGender);onPress();}}>
+          <Text  style={[
+            styles.NextButtonText,
+            sameGender
+              ? {color: colors.PRIMARY.DARK}
+              : {color: colors.GREY2},
+          ]}>같은 성별만</Text>
+        </Pressable>
+      </View>
+    </SafeContainer>
+  );
+};
+export default RoomSelectGender;
+
+const styles = StyleSheet.create({
+  textContainer: {
+    // paddingHorizontal: 28,
+  },
+  //   top: {
+  //     marginTop: 28,
+  //   },
+  title: {
+    color: '#212529',
+    fontSize: 24,
+    fontWeight: '700',
+    paddingLeft: 30,
+  },
+  subText: {
+    color: '#656E75',
+    fontSize: 16,
+    fontWeight: '400',
+    paddingBottom: 16,
+  },
+  strong: {
+    color: colors.PRIMARY.NORMAL,
+  },
+  buttonContainer: {
+    // position: 'absolute',
+    // bottom: '10%',
+    // width: '100%',
+    paddingHorizontal: 30,
+    paddingTop:34,
+  },
+  button: {
+    marginBottom: 16,
+  },
+  NextButton: {
+    width: 315,
+    height: 56,
+    borderRadius: 12,
+    backgroundColor: '#F2F4F8',
+    padding: 18,
+    alignItems: 'center',
+  },
+  NextButtonText: {
+    fontWeight: '700',
+    fontSize: 16,
+    color: '#A4ACB3',
+  },
+});
